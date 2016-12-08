940fc810c8ea61fd0f66d3808ad2e39c123b9ce2
Eric Werner <eric.werner09@gmail.com>
2016-12-08 15:34:31 -0600

    created and added MVC project to solution

---
+++ DontPanicAdventures/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DontPanicAdventures.Startup))]
+namespace DontPanicAdventures
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
